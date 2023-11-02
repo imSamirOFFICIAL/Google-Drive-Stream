fa0ed3a15cd1ecca2cc52b6bad373b2dc8e16c74
Samir <asamir192039@bscse.uiu.ac.bd>
2023-11-03 02:25:31 +0600

    Update

--- README.md
+++ README.md
@@ -78,7 +78,7 @@ For more option, see APlayer [options](https://aplayer.js.org/#/home?id=options)
 
 ## Change log
 
-### v2.0.10
+### v2.0.11
 
 - Fix image file actions does not work
 - Fix misjudged file to image format
--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
 var scripts = [
-  "https://cdn.jsdelivr.net/gh/imSamirOFFICIAL/Google-Drive-Stream@v2.0.10/dist/js/app.da7f5f3f.js",
-  "https://cdn.jsdelivr.net/gh/imSamirOFFICIAL/Google-Drive-Stream@v2.0.10/dist/js/chunk-vendors.3c185c97.js",
+  "https://cdn.jsdelivr.net/gh/imSamirOFFICIAL/Google-Drive-Stream@v2.0.11/dist/js/app.da7f5f3f.js",
+  "https://cdn.jsdelivr.net/gh/imSamirOFFICIAL/Google-Drive-Stream@v2.0.11/dist/js/chunk-vendors.3c185c97.js",
 ];
 var cdnjs = [
   "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js",
